01497fcedbfaa5ae42b68c9115d582bae012151b
Lorenzo Murarotto <lnzmrr@gmail.com>
2021-12-11 18:55:25 +0000

    implement API access

---
+++ frontend/src/api.ts
@@ -0,0 +1,32 @@
+export type Product = {
+  id: number;
+  code: string;
+  description: string;
+  price: number;
+  image_uri?: string;
+};
+
+export type Order = {
+  id: number;
+  created_at: Date;
+  notes?: string;
+  products: number[];
+};
+
+export async function getProduct(id: number) {
+  const result = await fetch(`/api/product/${id}`);
+  const product = await result.json();
+  return product as Product;
+}
+
+export async function getProducts(offset: number, count: number) {
+  const result = await fetch(`/api/products?offset=${offset}&count=${count}`);
+  const products = await result.json();
+  return products as Product[];
+}
+
+export async function getAllOrders() {
+  const result = await fetch("/api/allorders");
+  const orders = await result.json();
+  return orders as Order[];
+}
